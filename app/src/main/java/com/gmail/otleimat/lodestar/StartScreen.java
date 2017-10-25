e08351f1e699c0d125b2b29f22fbd091f37452bd
Omar Tleimat <otleimat@gmail.com>
2017-10-25 11:53:14 -0700

    Added option to turn Bluetooth on if not enabled

--- app/src/main/java/com/gmail/otleimat/lodestar/StartScreen.java
+++ app/src/main/java/com/gmail/otleimat/lodestar/StartScreen.java
@@ -3,27 +3,32 @@ package com.gmail.otleimat.lodestar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.bluetooth.*;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class StartScreen extends AppCompatActivity {
     private BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
+    private TextView startText;
+    private Button BluetoothOn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
-
+        startText = (TextView) findViewById(R.id.start_text);
+        BluetoothOn = (Button) findViewById(R.id.enable_bluetooth);
+        BluetoothOn.setVisibility(View.INVISIBLE);
         // Check if bluetooth is available and enabled on the device
         if(bluetooth != null)
         {
             if (bluetooth.isEnabled()) {
-                // Bluetooth Found
-                // Do something cool here
+                startText.setText("Bluetooth found and is enabled :)");
             }
             else
             {
-                // Bluetooth is not enabled
-                // Use cellular?
+                startText.setText("Bluetooth not enabled");
+
             }
         }
     }
