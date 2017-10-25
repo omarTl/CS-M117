3487dada195be867c0c374063c6b85729d8bbf75
Omar Tleimat <otleimat@gmail.com>
2017-10-25 12:56:44 -0700

    Removed useless imports for now..

--- app/src/main/java/com/gmail/otleimat/lodestar/VictimScreen.java
+++ app/src/main/java/com/gmail/otleimat/lodestar/VictimScreen.java
@@ -2,9 +2,7 @@ package com.gmail.otleimat.lodestar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
 
 /**
  * Created by omart on 10/25/2017.
