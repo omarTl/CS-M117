8c4a63f1385d28656d0fcfcc9a56de41f1b9044c
Omar Tleimat <otleimat@gmail.com>
2017-10-25 11:35:50 -0700

    Added check for bluetooth

--- app/src/main/java/com/gmail/otleimat/lodestar/StartScreen.java
+++ app/src/main/java/com/gmail/otleimat/lodestar/StartScreen.java
@@ -3,12 +3,28 @@ package com.gmail.otleimat.lodestar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.bluetooth.*;
+import android.widget.TextView;
+
 
 public class StartScreen extends AppCompatActivity {
-
+    private BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        // Check if bluetooth is available and enabled on the device
+        if(bluetooth != null)
+        {
+            if (bluetooth.isEnabled()) {
+                // Bluetooth Found
+                // Do something cool here
+            }
+            else
+            {
+                // Bluetooth is not enabled
+                // Use cellular?
+            }
+        }
     }
 }
